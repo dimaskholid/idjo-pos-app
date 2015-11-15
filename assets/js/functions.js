d58b08ca9a1d6dad31ca610ed22e085970b41371
Dimas Kholid <dimas.kholid@gmail.com>
2015-11-16 00:02:28 +0700

    update report_ , css-print

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -141,8 +141,21 @@ function setYear(selectYear) {
     $(selectYear).val(curYear.getFullYear());
 }
 
-
-
+/** accounting setting**/
+accounting.settings = {
+    currency: {
+        symbol: "Rp. ", // default currency symbol is '$'
+        format: "%s%v", // controls output: %s = symbol, %v = value/number (can be object: see below)
+        decimal: ",", // decimal point separator
+        thousand: ".", // thousands separator
+        precision: 0 // decimal places
+    },
+    number: {
+        precision: 0, // default precision on numbers is 0
+        thousand: ".",
+        decimal: ","
+    }
+}
 
 
 
